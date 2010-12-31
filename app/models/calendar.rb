417d80dfdbea9e3c0880f11310117a376293be93
Alexander Khitev <alexander.khitev@gmail.com>
2010-12-31 17:13:31 +0300

    Added changing feed: basic -> full

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -18,6 +18,12 @@ def pull_from_feed(xml)
 end
 
   def self.create_from_feed( xml_feed)
+    if xml_feed.end_with?('basic')
+      #changing feed type as 'basic' doesnot had enough info 
+      i = xml_feed.length() -6
+      xml_feed = xml_feed[0..i] + 'full'
+      puts xml_feed
+    end
     http = Net::HTTP.get_response(URI.parse(xml_feed)).body    
     calendar = Calendar.new(Nokogiri::XML(http))
     calendar.xml_feed= xml_feed
